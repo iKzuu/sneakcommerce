d4662cae1601eadafd6fabcb33f1d9192a95273b
iKzuu <anangsetiaji30@gmail.com>
2026-03-07 21:00:42 +0700

    change app color

--- lib/components/searchbar.dart
+++ lib/components/searchbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sneakcommerce/controller/controller.dart';
+import 'package:sneakcommerce/theme/app_colors.dart';
 
 class Searchbar extends StatefulWidget {
   final TextEditingController controller;
@@ -17,17 +18,17 @@ class _SearchbarState extends State<Searchbar> {
       height: 40,
       child: TextField(
         cursorHeight: 18,
-        cursorColor: Color(0xFFC9BCAF),
+        cursorColor: AppColors.onSurface,
         controller: widget.controller,
         decoration: InputDecoration(
           filled: true,
-          fillColor: Colors.white,
+          fillColor: AppColors.background,
           contentPadding: EdgeInsets.all(8),
           prefixIcon: Icon(Icons.search, size: 20, color: Color(0xFF435150)),
           hintText: "search seneakers...",
           hintStyle: TextStyle(
             fontSize: 14,
-            color: Colors.black.withValues(alpha: 0.3),
+            color: AppColors.onSurfaceVariant,
             fontWeight: FontWeight.w600,
           ),
           border: OutlineInputBorder(
@@ -35,7 +36,10 @@ class _SearchbarState extends State<Searchbar> {
             borderSide: BorderSide.none,
           ),
         ),
-        onChanged: (value) => Provider.of<Controller>(context, listen: false).setSearchQuery(value),
+        onChanged: (value) => Provider.of<Controller>(
+          context,
+          listen: false,
+        ).setSearchQuery(value),
       ),
     );
   }
