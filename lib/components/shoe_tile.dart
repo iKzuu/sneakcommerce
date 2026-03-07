0320acf0da19e9fccf6551f5d9f5a148de4b4815
iKzuu <anangsetiaji30@gmail.com>
2026-03-07 20:59:51 +0700

    change app color and refactor some code

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -1,146 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 import 'package:sneakcommerce/controller/controller.dart';
 import 'package:sneakcommerce/models/shoe.dart';
 import 'package:sneakcommerce/pages/detail_page.dart';
+import 'package:sneakcommerce/theme/app_colors.dart';
+import 'package:sneakcommerce/utils/idr_formatter.dart';
 
-class ShoeTile extends StatefulWidget {
+class ShoeTile extends StatelessWidget {
   Shoe shoe;
-  int shoeId;
-  ShoeTile({super.key, required this.shoe, required this.shoeId});
-
-  @override
-  State<ShoeTile> createState() => _ShoeTileState();
-}
-
-class _ShoeTileState extends State<ShoeTile> {
-  void addShoeToCart(Shoe shoe) {
-    Provider.of<Controller>(context, listen: false).addItemToCart(shoe);
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        backgroundColor: Color(0xFF435150),
-        title: Text(
-          "Added 1 item to cart",
-          style: TextStyle(color: Colors.white),
-        ),
-        content: Text("Check your cart", style: TextStyle(color: Colors.white)),
-      ),
-    );
-  }
+  ShoeTile({super.key, required this.shoe});
 
   @override
   Widget build(BuildContext context) {
     return Consumer<Controller>(
       builder: (context, value, child) {
-        final shoe = value.shoeShopList.firstWhere(
-          (s) => s.id == widget.shoeId,
-        );
+        final shoes = value.shoeShopList.firstWhere((s) => s.id == shoe.id);
 
         return GestureDetector(
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => DetailPage(shoeId: shoe.id),
+                builder: (context) => DetailPage(shoeId: shoes.id),
               ),
             );
           },
           child: Container(
             decoration: BoxDecoration(
-              color: Color(0xFFF5F5F5),
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(6),
             ),
-            child: Stack(
+            child: Column(
+              spacing: 4,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.all(12),
-                  child: SizedBox(
-                    width: 160,
-                    height: 160,
-                    child: Image.network(
-                      widget.shoe.imagePath,
-                      fit: BoxFit.cover,
+                SizedBox(
+                  width: double.infinity,
+                  height: 180,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(6),
+                      topRight: Radius.circular(6),
                     ),
+                    child: Image.network(shoes.imagePath, fit: BoxFit.cover),
                   ),
                 ),
 
-                Positioned(
-                  bottom: 12,
-                  left: 12,
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  width: double.infinity,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        widget.shoe.name,
+                        shoes.name,
                         style: TextStyle(
-                          color: Color(0xFF242424),
+                          color: AppColors.onSurface,
                           fontWeight: FontWeight.w600,
                           fontSize: 14,
                         ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.shoe.brand,
-                            style: TextStyle(
-                              color: Color(0xFF464646).withValues(alpha: 0.6),
-                              fontSize: 12,
-                            ),
-                          ),
+                      Text(
+                        shoes.brand,
+                        style: TextStyle(
+                          color: AppColors.onSurfaceVariant,
+                          fontSize: 12,
+                        ),
+                      ),
 
-                          Text(
-                            widget.shoe.price,
-                            style: TextStyle(
-                              color: Color(0xFF464646),
-                              fontWeight: FontWeight.w600,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        formatRupiah(shoes.price),
+                        style: TextStyle(
+                          color: AppColors.onSurface,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12,
+                        ),
                       ),
                     ],
-                  ),
-                ),
-
-                Positioned(
-                  bottom: 12,
-                  right: 12,
-                  child: GestureDetector(
-                    onTap: () => addShoeToCart(shoe),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF435150),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Icon(
-                        Iconsax.shopping_bag5,
-                        color: Colors.white,
-                        size: 18,
-                      ),
-                    ),
-                  ),
-                ),
-
-                Positioned(
-                  top: 12,
-                  right: 12,
-                  child: IconButton(
-                    onPressed: () {
-                      value.toggleFavorite(shoe.id);
-                    },
-
-                    icon: shoe.isFavorite
-                        ? Icon(Icons.favorite_rounded, color: Color(0xFF435150))
-                        : Icon(
-                            Icons.favorite_border_rounded,
-                            color: Color(0xFF435150),
-                          ),
                   ),
                 ),
               ],
