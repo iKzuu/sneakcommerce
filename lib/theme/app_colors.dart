3215f8b90249eb7f72cbcf961100c0d24b4b385d
iKzuu <anangsetiaji30@gmail.com>
2026-03-07 21:01:02 +0700

    app color theme

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFFFFFFF);
+  static const onBackground = Color(0xFF000000);
+
+  static const surface = Color(0xFFF5F5F5);
+  static const onSurface = Color(0xFF000000);
+
+  static const onSurfaceVariant = Color(0xFF707074);
+}
