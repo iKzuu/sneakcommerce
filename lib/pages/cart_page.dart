92a4ef6f30fdb3e6cabedfdb2cf918ac1bcd9de9
iKzuu <anangsetiaji30@gmail.com>
2026-04-07 19:55:22 +0700

    feat: enhance cart page layout with total price display and checkout button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 import 'package:sneakcommerce/components/list_items.dart';
-import 'package:sneakcommerce/components/page_heading.dart';
 import 'package:sneakcommerce/controller/controller.dart';
+import 'package:sneakcommerce/theme/app_colors.dart';
+import 'package:sneakcommerce/utils/idr_formatter.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -27,10 +27,6 @@ class _CartPageState extends State<CartPage> {
       ),
       body: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
-            child: PageHeading(text: "Cart", icon: Iconsax.shopping_bag5),
-          ),
-
           Consumer<Controller>(
             builder: (context, value, index) {
               return ListItems(
@@ -39,10 +35,78 @@ class _CartPageState extends State<CartPage> {
                 },
                 items: value.userCartItem,
                 isCart: true,
+                imageWidth: 80,
+                imageHeight: 80,
               );
             },
           ),
         ],
+      ),
+
+      bottomNavigationBar: Consumer<Controller>(
+        builder: (context, value, child) {
+          if (value.userCartItem.isEmpty) return const SizedBox.shrink();
+
+          return Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(color: AppColors.onSurface),
+            child: SafeArea(
+              child: Row(
+                spacing: 20,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Total",
+                        style: TextStyle(
+                          color: AppColors.onSurfaceVariant,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        formatRupiah(value.getTotalPrice()),
+                        style: TextStyle(
+                          color: AppColors.surface,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      surfaceTintColor: Colors.transparent,
+                      elevation: 0,
+                      backgroundColor: AppColors.surface,
+                      shadowColor: AppColors.onSurfaceVariant,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(4),
+                      ),
+                    ),
+                    child: const Text(
+                      "Checkout",
+                      style: TextStyle(
+                        color: AppColors.onSurface,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
