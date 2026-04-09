4a5ae57e89fc3b762ad408efa1af69e04d8b9ae9
iKzuu <anangsetiaji30@gmail.com>
2026-04-10 00:51:29 +0700

    feat: create ShippingMethod model for managing shipping options

---
+++ lib/models/shipping.dart
@@ -0,0 +1,13 @@
+class ShippingMethod {
+  final String id;
+  final String name;
+  final int cost;
+  final String estimatedDelivery;
+
+  ShippingMethod({
+    required this.id,
+    required this.name,
+    required this.cost,
+    required this.estimatedDelivery,
+  });
+}
