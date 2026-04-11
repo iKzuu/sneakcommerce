88a266065fb0f230dc1ae7da6f127263938fe8ab
iKzuu <anangsetiaji30@gmail.com>
2026-04-11 23:14:26 +0700

    feat: add payment method data with multiple payment options

---
+++ lib/data/payment_method_data.dart
@@ -0,0 +1,39 @@
+import 'package:sneakcommerce/models/payment_method.dart';
+
+List<PaymentMethod> paymentMethods = [
+  PaymentMethod(
+    id: "pmyt_dana",
+    name: "Dana",
+    imageUrl: "assets/icons/payment-logo/dana_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_gopay",
+    name: "Gopay",
+    imageUrl: "assets/icons/payment-logo/gopay_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_shopeepay",
+    name: "ShopeePay",
+    imageUrl: "assets/icons/payment-logo/shopeepay_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_linkaja",
+    name: "LinkAja!",
+    imageUrl: "assets/icons/payment-logo/linkaja_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_seabank",
+    name: "SeaBank",
+    imageUrl: "assets/icons/payment-logo/seabank_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_brimo",
+    name: "Brimo",
+    imageUrl: "assets/icons/payment-logo/brimo_logo.png",
+  ),
+  PaymentMethod(
+    id: "pmyt_livin",
+    name: "Livin' by Mandiri",
+    imageUrl: "assets/icons/payment-logo/livinbymandiri_logo.png",
+  ),
+];
