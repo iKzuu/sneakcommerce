b1241433352064af667aa8d5eaebe6d87b6200dd
iKzuu <anangsetiaji30@gmail.com>
2026-04-11 23:14:36 +0700

    feat: create PaymentMethod model to represent payment options

---
+++ lib/models/payment_method.dart
@@ -0,0 +1,11 @@
+class PaymentMethod {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  PaymentMethod({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+}
