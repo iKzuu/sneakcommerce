4fc3f47f608234b34d4c3d634c65411329c717af
iKzuu <anangsetiaji30@gmail.com>
2026-04-29 21:27:33 +0700

    feat: replace shipping address section with AddressSection component

--- lib/pages/chekout_page.dart
+++ lib/pages/chekout_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneakcommerce/components/checkout_page_sections/address_section.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/checkout_items.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/payment_list.dart';
 import 'package:sneakcommerce/theme/app_colors.dart';
@@ -13,6 +14,8 @@ class CheckoutPage extends StatelessWidget {
       appBar: AppBar(
         foregroundColor: AppColors.onSurface,
         backgroundColor: AppColors.surface,
+        elevation: 0,
+        surfaceTintColor: Colors.transparent,
         title: const Text(
           "Checkout",
           style: TextStyle(color: AppColors.onSurface),
@@ -28,71 +31,7 @@ class CheckoutPage extends StatelessWidget {
           spacing: 10,
           children: [
             // shipping address section
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              width: double.infinity,
-              decoration: BoxDecoration(color: AppColors.surface),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Column(
-                      spacing: 6,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Your shipping address",
-                          style: TextStyle(
-                            color: AppColors.onSurfaceVariant,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          spacing: 4,
-                          children: [
-                            Icon(
-                              Icons.location_pin,
-                              color: Colors.lightBlueAccent,
-                            ),
-                            Expanded(
-                              child: Text(
-                                "My Home | Your Name",
-                                style: TextStyle(
-                                  color: AppColors.onSurface,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "Jl. Contoh Alamat No. 123, Jakarta, Indonesia",
-                          style: TextStyle(
-                            color: AppColors.onSurfaceVariant,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: AppColors.onSurfaceVariant,
-                    size: 18,
-                  ),
-                ],
-              ),
-            ),
+            AddressSection(),
 
             // checkout items section & shipping method section
             CheckoutItems(),
