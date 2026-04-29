3175d650a8b80a11052ab437f0dc0724e270850b
iKzuu <anangsetiaji30@gmail.com>
2026-04-29 21:24:41 +0700

    feat: add confirmation dialog utility method to DialogUtils

--- lib/utils/dialog_utils.dart
+++ lib/utils/dialog_utils.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sneakcommerce/theme/app_colors.dart';
 
 class DialogUtils {
+
+  // information dialog
   static void showInfoDialog(
     BuildContext context,
     String title,
@@ -32,6 +34,58 @@ class DialogUtils {
               child: const Text(
                 "Okie Dokie",
                 style: TextStyle(color: AppColors.surface),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // confirmation dialog
+  static void showConfirmDialog({
+    required BuildContext context,
+    required String title,
+    required String content,
+    required VoidCallback onConfirm,
+    String confirmText = "Delete",
+    String cancelText = "Cancel",
+  }) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: AppColors.onBackground,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+              color: AppColors.surface,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: Text(
+            content,
+            style: const TextStyle(color: AppColors.surface),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                cancelText,
+                style: const TextStyle(color: AppColors.surface),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // tutup dialog
+                onConfirm(); // jalankan aksi
+              },
+              child: Text(
+                confirmText,
+                style: const TextStyle(color: Colors.red),
               ),
             ),
           ],
