050b7e85b79d23c26f10ac4f5f7d4546227ec193
iKzuu <anangsetiaji30@gmail.com>
2026-04-30 20:40:05 +0700

    feat: add CheckoutPage to implement the checkout process with address, items, payment, and order details sections

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:sneakcommerce/components/checkout_page_sections/address_section.dart';
+import 'package:sneakcommerce/components/checkout_page_sections/checkout_items.dart';
+import 'package:sneakcommerce/components/checkout_page_sections/order_details.dart';
+import 'package:sneakcommerce/components/checkout_page_sections/payment_list.dart';
+import 'package:sneakcommerce/theme/app_colors.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        foregroundColor: AppColors.onSurface,
+        backgroundColor: AppColors.surface,
+        elevation: 0,
+        surfaceTintColor: Colors.transparent,
+        title: const Text(
+          "Checkout",
+          style: TextStyle(color: AppColors.onSurface),
+        ),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 10,
+          children: [
+            // shipping address section
+            AddressSection(),
+
+            // checkout items section & shipping method section
+            CheckoutItems(),
+
+            // payment method section
+            PaymentList(),
+
+            // order details section
+            OrderDetails(),
+          ],
+        ),
+      ),
+    );
+  }
+}
