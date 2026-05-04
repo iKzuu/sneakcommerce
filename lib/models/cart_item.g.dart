75988fd79407988f6854ee4602bc2f3aa8f2b87e
iKzuu <anangsetiaji30@gmail.com>
2026-05-04 21:31:24 +0700

    feat: add CartItemAdapter for serializing and deserializing CartItem data

---
+++ lib/models/cart_item.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_item.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CartItemAdapter extends TypeAdapter<CartItem> {
+  @override
+  final int typeId = 1;
+
+  @override
+  CartItem read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CartItem(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      price: fields[2] as int,
+      imagePath: fields[3] as String,
+      brand: fields[4] as String,
+      quantity: fields[5] as int,
+      selectedSize: fields[6] as String?,
+      isSelected: fields[7] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CartItem obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.imagePath)
+      ..writeByte(4)
+      ..write(obj.brand)
+      ..writeByte(5)
+      ..write(obj.quantity)
+      ..writeByte(6)
+      ..write(obj.selectedSize)
+      ..writeByte(7)
+      ..write(obj.isSelected);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CartItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
