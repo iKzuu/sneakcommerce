144f703e95c81981f18478a50ff2863b015e1b8f
iKzuu <anangsetiaji30@gmail.com>
2026-05-04 21:30:59 +0700

    feat: update AppProviders to include ShoeController, CartController, WishlistController, and ShoeSearchController

--- lib/core/app_providers.dart
+++ lib/core/app_providers.dart
@@ -1,15 +1,21 @@
 import 'package:provider/provider.dart';
 import 'package:sneakcommerce/controller/address_controller.dart';
-import 'package:sneakcommerce/controller/controller.dart';
+import 'package:sneakcommerce/controller/cart_controller.dart';
+import 'package:sneakcommerce/controller/shoe_controller.dart';
 import 'package:sneakcommerce/controller/payment_method_controller.dart';
 import 'package:sneakcommerce/controller/shipping_controller.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:sneakcommerce/controller/shoe_search_controller.dart';
+import 'package:sneakcommerce/controller/wishlist_controller.dart';
 
 class AppProviders {
   static List<SingleChildWidget> providers = [
-    ChangeNotifierProvider(create: (_) => Controller()),
+    ChangeNotifierProvider(create: (_) => ShoeController()),
     ChangeNotifierProvider(create: (_) => ShippingController()),
     ChangeNotifierProvider(create: (_) => PaymentMethodController()),
     ChangeNotifierProvider(create: (_) => AddressController()),
+    ChangeNotifierProvider(create: (_) => CartController()),
+    ChangeNotifierProvider(create: (_) => WishlistController()),
+    ChangeNotifierProvider(create: (_) => ShoeSearchController()),
   ];
 }
