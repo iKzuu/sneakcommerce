04838940c8186ac9068b6d1d80bf74376366397a
iKzuu <anangsetiaji30@gmail.com>
2026-05-04 21:30:00 +0700

    feat: add ProductCard component for displaying individual shoe details with image and actions

---
+++ lib/components/cards/product_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneakcommerce/controller/shoe_controller.dart';
+import 'package:sneakcommerce/models/shoe.dart';
+import 'package:sneakcommerce/pages/detail_page.dart';
+import 'package:sneakcommerce/theme/app_colors.dart';
+import 'package:sneakcommerce/utils/idr_formatter.dart';
+
+class ProductCard extends StatelessWidget {
+  final Shoe shoe;
+  final double? imageWidth;
+  final double? imageHeight;
+  const ProductCard({
+    super.key,
+    required this.shoe,
+    this.imageWidth = 110, // padahal di sini udah ada nilai default
+    this.imageHeight = 110, // padahal di sini udah ada nilai default
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ShoeController>(
+      builder: (context, value, _) {
+        final displayShoe = shoe;
+
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailPage(shoeId: displayShoe.id),
+              ),
+            );
+          },
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(color: AppColors.surface),
+            child: Row(
+              spacing: 8,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: imageWidth,
+                  height: imageHeight,
+                  child: Image.network(
+                    displayShoe.imagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                Expanded(
+                  child: Column(
+                    spacing: 6,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        displayShoe.name,
+                        style: TextStyle(
+                          color: AppColors.onSurface,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+                      Text(
+                        displayShoe.brand,
+                        style: TextStyle(color: AppColors.onSurfaceVariant),
+                      ),
+
+                      Text(
+                        formatRupiah(displayShoe.price),
+                        style: TextStyle(
+                          color: AppColors.onSurface,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
