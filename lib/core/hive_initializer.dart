bf3409714a74cd3116f2b175664c510eb18f9327
iKzuu <anangsetiaji30@gmail.com>
2026-05-04 21:31:08 +0700

    feat: register CartItemAdapter and open cartBox in HiveInitializer

--- lib/core/hive_initializer.dart
+++ lib/core/hive_initializer.dart
@@ -1,14 +1,17 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sneakcommerce/models/address.dart';
+import 'package:sneakcommerce/models/cart_item.dart';
 
 class HiveInitializer {
   static Future<void> init() async {
     await Hive.initFlutter();
 
     Hive.registerAdapter(AddressAdapter());
+    Hive.registerAdapter(CartItemAdapter());
 
     await Future.wait([
       Hive.openBox<Address>('addressBox'),
+      Hive.openBox<CartItem>('cartBox'),
     ]);
   }
 }
