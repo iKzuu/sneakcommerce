ae7796596359f151d0c655c8bd9e73dc3a157904
iKzuu <anangsetiaji30@gmail.com>
2026-05-06 14:54:47 +0700

    feat: register Order and OrderItem adapters in HiveInitializer for order management

--- lib/core/hive_initializer.dart
+++ lib/core/hive_initializer.dart
@@ -1,6 +1,8 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sneakcommerce/models/address.dart';
 import 'package:sneakcommerce/models/cart_item.dart';
+import 'package:sneakcommerce/models/order.dart';
+import 'package:sneakcommerce/models/order_item.dart';
 
 class HiveInitializer {
   static Future<void> init() async {
@@ -8,11 +10,14 @@ class HiveInitializer {
 
     Hive.registerAdapter(AddressAdapter());
     Hive.registerAdapter(CartItemAdapter());
+    Hive.registerAdapter(OrderItemAdapter());
+    Hive.registerAdapter(OrderAdapter());
 
     await Future.wait([
       Hive.openBox<Address>('addressBox'),
       Hive.openBox<CartItem>('cartBox'),
       Hive.openBox<int>('wishlistBox'),
+      Hive.openBox<Order>('orderBox')
     ]);
   }
 }
