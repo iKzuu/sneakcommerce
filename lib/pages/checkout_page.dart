67ad118e15c5d2fde167960f7d23841680fa9b54
iKzuu <anangsetiaji30@gmail.com>
2026-05-06 14:55:33 +0700

    feat: implement checkout functionality with order placement button

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/address_section.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/checkout_items.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/order_details.dart';
 import 'package:sneakcommerce/components/checkout_page_sections/payment_list.dart';
+import 'package:sneakcommerce/controller/address_controller.dart';
+import 'package:sneakcommerce/controller/cart_controller.dart';
+import 'package:sneakcommerce/controller/order_controller.dart';
+import 'package:sneakcommerce/controller/payment_method_controller.dart';
+import 'package:sneakcommerce/controller/shipping_controller.dart';
+import 'package:sneakcommerce/pages/home_page.dart';
 import 'package:sneakcommerce/theme/app_colors.dart';
 
 class CheckoutPage extends StatelessWidget {
@@ -43,6 +50,82 @@ class CheckoutPage extends StatelessWidget {
             // order details section
             OrderDetails(),
           ],
+        ),
+      ),
+
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          height: 55,
+          child:
+              Consumer4<
+                CartController,
+                AddressController,
+                PaymentMethodController,
+                ShippingController
+              >(
+                builder:
+                    (
+                      context,
+                      cartController,
+                      addressController,
+                      paymentController,
+                      shippingController,
+                      _,
+                    ) {
+                      return ElevatedButton(
+                        onPressed: () async {
+                          final success = await context
+                              .read<OrderController>()
+                              .checkout(
+                                cartController: context.read<CartController>(),
+                                addressController: context
+                                    .read<AddressController>(),
+                                paymentController: context
+                                    .read<PaymentMethodController>(),
+                                shippingController: context
+                                    .read<ShippingController>(),
+                              );
+
+                          if (!success) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Please complete checkout'),
+                              ),
+                            );
+
+                            return;
+                          }
+
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const HomePage(initialIndex: 1),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                        
+                        style: ElevatedButton.styleFrom(
+                          surfaceTintColor: Colors.transparent,
+                          elevation: 0,
+                          backgroundColor: AppColors.onBackground,
+                          shadowColor: AppColors.onSurfaceVariant,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(4),
+                          ),
+                        ),
+                        child: Text(
+                          "Place Order",
+                          style: TextStyle(
+                            color: AppColors.surface,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                    },
+              ),
         ),
       ),
     );
