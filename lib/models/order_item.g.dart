3f69e1a93a62932e03032705ff5ea727bd3ba899
iKzuu <anangsetiaji30@gmail.com>
2026-05-06 14:55:11 +0700

    feat: add OrderItemAdapter for serialization and deserialization of OrderItem

---
+++ lib/models/order_item.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order_item.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OrderItemAdapter extends TypeAdapter<OrderItem> {
+  @override
+  final int typeId = 2;
+
+  @override
+  OrderItem read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OrderItem(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      price: fields[2] as int,
+      imagePath: fields[3] as String,
+      quantity: fields[4] as int,
+      selectedSize: fields[5] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OrderItem obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.imagePath)
+      ..writeByte(4)
+      ..write(obj.quantity)
+      ..writeByte(5)
+      ..write(obj.selectedSize);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OrderItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
