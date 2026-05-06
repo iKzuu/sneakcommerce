b31467f601ec5eaa2443628100cf9413023d0fc8
iKzuu <anangsetiaji30@gmail.com>
2026-05-06 14:55:45 +0700

    feat: update OrdersPage to display a list of orders using OrderController

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneakcommerce/controller/order_controller.dart';
 
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Orders Page"),
+    return Consumer<OrderController>(
+      builder: (context, controller, _) {
+        final orders = controller.orders;
+
+        return ListView.builder(
+          itemCount: orders.length,
+          itemBuilder: (_, index) {
+            final order = orders[index];
+
+            return Text(order.orderId);
+          },
+        );
+      },
     );
   }
 }
